b6b47d2b4959fb9a751e3a71d46b131c3f5ec044
changwinstonr <chang.winstonr@gmail.com>
2016-04-24 03:22:33 -0700

    Debugs resources. Notifies SDK is out of date. Updating...

--- app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/MainActivity.java
+++ app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/MainActivity.java
@@ -5,14 +5,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView[] mBlocks = new ImageView[9];
+//    private TextView mDisplay;
+    private enum TURN {CIRCLE, CROSS}
+    private TURN mTurn;
+    private int mStatusCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void init(){
 
     }
+
 }
