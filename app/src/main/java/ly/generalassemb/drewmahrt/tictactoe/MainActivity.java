e9e09b21bb705d476b6934dafba145611d9f6ac9
changwinstonr <chang.winstonr@gmail.com>
2016-04-25 23:35:28 -0700

    Moves the forLoop out of the onClick.

--- app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/MainActivity.java
+++ app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/MainActivity.java
@@ -6,29 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-//import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView[] mBlocks = new ImageView[9];
-    private TextView mDisplay; //maybe add Toast just below game_turn_text on player change
-    private enum TURN {CIRCLE, CROSS}
-    private TURN mTurn;
-    private ImageView mReplay;
-    private int mStatusCounter = 0;
 
-/*
-
-p1 et fv rid p1n;
-    "" +
-            "" +
-            int int = new int maincthis gameactclass;
-            inten.putE "pa",pa.getT().getStr();
-            "p1gt new string p1.gettext.tostroing" +;
-            "" +
-            ""
-
-            */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,19 +18,29 @@ p1 et fv rid p1n;
         init();
     }
 
-    private void init(){
-        mDisplay = (TextView) findViewById(R.id.game_turn_text);
-        mReplay = (ImageView) findViewById(R.id.playAgain);
-        mReplay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent starter = getIntent();
-                finish();
-                //starter.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                startActivity(starter);
-            }
+    private ImageView[] mBlocks = new ImageView[9];
+    private TextView mDisplay;
+    private enum TURN {CIRCLE, CROSS}
+    private TURN mTurn;
+    private ImageView mReplay;
+    private int mStatusCounter = 0;
 
-            for(int position = 0; position < 9; position++) {
+        private void init() {
+            mDisplay = (TextView) findViewById(R.id.game_turn_text);
+            mReplay = (ImageView) findViewById(R.id.playAgain);
+            mReplay.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent starter = getIntent();
+                    finish();
+
+                    startActivity(starter);
+                }
+            });
+        }
+
+    //onClick ImageViews
+            for(int position = 0; position < 9;  position++){
                 int resId = getResources().getIdentifier("block_" + (position + 1), "id", getPackageName());
                 mBlocks[position] = (ImageView) findViewById(resId);
                 final int finalPosition = position;
@@ -60,25 +51,28 @@ p1 et fv rid p1n;
                     }
                 });
             }
+            //change turns
             private void switchTurn(int position) {
                 if (mTurn == TURN.CIRCLE) {
                     mBlocks[position].setImageResource(R.drawable.caboose_circle);
                     mBlocks[position].setId(GameActivity.CIRCLE);
                     mTurn = TURN.CROSS;
-                    mDisplay.setText("CROSS's turn");
+                    mDisplay.setText(position);
                 } else {
                     mBlocks[position].setImageResource(R.drawable.sarge_cross);
                     mBlocks[position].setId(GameActivity.CROSS);
                     mTurn = TURN.CIRCLE;
-                    mDisplay.setText("CIRCLE's turn");
+                    mDisplay.setText(position);
                 }
+
                 mBlocks[position].setEnabled(false);
                 mStatusCounter++;
+
                 if (GameActivity.hasWon(position + 1, mBlocks)) {
                     mDisplay.setText(GameActivity.mWinner + " won");
                     displayStick(GameActivity.mSet);
                     disableAll();
-                }else if (mStatusCounter==9){
+                }else if (mStatusCounter==9) {
                     mDisplay.setText("WORT WORT WORT. Try Again.");
                 }
             }
@@ -120,12 +114,4 @@ p1 et fv rid p1n;
                 for (int i = 0; i < 9; i++)
                     mBlocks[i].setEnabled(false);
             }
-
-
-        });
     }
-
-
-
-
-}
